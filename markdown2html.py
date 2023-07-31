c866bbf8f54ed019ab41224ca0820ceb59b8943a
rChrisb <5767@holbertonstudents.com>
2023-07-31 15:43:21 +0200

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """some script to start"""
+
+
 import sys
 import os
 
-if len(sys.argv) < 3:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-    sys.exit(1)
-
-if not os.path.isfile(sys.argv[1]):
-    sys.stderr.write(f"Missing {sys.argv[1]}\n")
-    sys.exit(1)
-
-sys.exit(0)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
+
+    if not os.path.isfile(sys.argv[1]):
+        sys.stderr.write(f"Missing {sys.argv[1]}\n")
+        sys.exit(1)
